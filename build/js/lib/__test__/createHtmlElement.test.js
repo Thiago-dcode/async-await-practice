89282cc85158996b0ff97f2cef0ae09197dfb54a
Thiago-dcode <contact.thiago.ferreira@gmail.com>
2023-06-06 12:46:49 +0200

    fixing module error

--- build/js/lib/__test__/createHtmlElement.test.js
+++ build/js/lib/__test__/createHtmlElement.test.js
@@ -1,16 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createHtmlElement_1 = require("../createHtmlElement");
+import createHtmlElement from "../createHtmlElement";
 describe('CreateHtmlElement function', () => {
     test('should return a html element', () => {
         const tag = 'div';
-        const htmlElement = (0, createHtmlElement_1.createHtmlElement)(tag);
+        const htmlElement = createHtmlElement(tag);
         expect(htmlElement.tagName).toBe(tag.toUpperCase());
         expect(htmlElement instanceof HTMLElement).toBe(true);
     });
     test('should assign the props passed to the html element ', () => {
         const tag = 'div';
-        const htmlElement = (0, createHtmlElement_1.createHtmlElement)(tag, {
+        const htmlElement = createHtmlElement(tag, {
             'textContent': 'hello world'
         });
         expect(htmlElement.textContent).toBe('hello world');
